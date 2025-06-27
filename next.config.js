453f1127f915e3d5064818369e50bbcbe61f044a
Luan Victor <luan.silva@dsgtechnology.com.br>
2025-06-27 17:06:09 -0300

    fix: tentando corrigir deploy na vercel

--- next.config.js
+++ next.config.js
@@ -1,18 +1,37 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../"),
+  output: "standalone",
+
+  images: {
+    domains: [],
+    unoptimized: false,
   },
+
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
+
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
